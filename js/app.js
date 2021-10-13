452d1e1dc57a8a98ed3459b91fc6c5f6ece13564
ala2020-cyber <73254102+ala2020-cyber@users.noreply.github.com>
2021-10-14 00:26:13 +0100

    update in js+ css

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const loding= document.querySelector('.loding');
-
+const html_elem=document.querySelector('html');
 window.addEventListener('load' , () => {
-    loding.classList.add('vanish-away')
+    loding.classList.add('vanish-away');
+    html_elem.classList.add('showing');
 })
