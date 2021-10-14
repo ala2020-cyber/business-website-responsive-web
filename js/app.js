a8fb5aa7ff4a42be6f6751d291ce935c134080cb
ala2020-cyber <73254102+ala2020-cyber@users.noreply.github.com>
2021-10-14 01:48:54 +0100

    solving loding div litle trouble

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const loding= document.querySelector('.loding');
+
 const html_elem=document.querySelector('html');
+/* loding listner */
 window.addEventListener('load' , () => {
     loding.classList.add('vanish-away');
+    loding.classList.add('loding-place');
     html_elem.classList.add('showing');
 })
