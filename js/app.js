205470eded58e72db4044581a97a593df3ccfa7a
Debabi Ala <73254102+ala2020-cyber@users.noreply.github.com>
2022-03-21 22:23:43 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const loading= document.querySelector('.loading');
+const html_body=document.querySelector('html');
 /* loding listner */
 window.addEventListener('load' , () => {
     loading.classList.add('vanish-away');
     loading.style.display="none";
+    html_body.style.overflow="scroll";
+    html_body.style.overflowX="hidden";
 })
